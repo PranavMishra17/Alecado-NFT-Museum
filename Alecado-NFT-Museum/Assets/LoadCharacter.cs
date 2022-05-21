cbf8115d81e97a2e18d3c7aa00b2256749b0d616
Pranav Mishra <pranavmishra.fc17@gmail.com>
2022-05-21 18:23:36 +0530

    Character Selection added

---
+++ Alecado-NFT-Museum/Assets/LoadCharacter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadCharacter : MonoBehaviour
+{
+    public GameObject[] characterPrefabs;
+    public Transform spawnPoint;
+    public Text chrName;
+    public Text chrRole;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int selectedCharacter = PlayerPrefs.GetInt("selectedCharacter");
+        GameObject prefab = characterPrefabs[selectedCharacter];
+        GameObject clone = Instantiate(prefab, spawnPoint.position, Quaternion.identity);
+        chrName.text = PlayerPrefs.GetString("characterName");
+        chrRole.text = PlayerPrefs.GetString("playerRole");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
